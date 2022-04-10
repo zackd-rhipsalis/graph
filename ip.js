9d96c5e6272be55144a7a54c5d98f1eaba4c4152
rhipsali <89015741+rhipsali@users.noreply.github.com>
2022-04-11 05:37:59 +0900

    push

--- ip.js
+++ ip.js
@@ -4,12 +4,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  const resData = [];
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.connection.socket.remoteAddress || req.socket.remoteAddress || '0.0.0.0', 
   str = (ip.match(/[^0-9.]/g)) ? ip.replace(/[^0-9.]/g, "") : ip;
   console.log(`IP: ${str}`);
-  resData.push(str);
-  res.send(JSON.stringify(resData[0]));
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log("listening on " + port));
