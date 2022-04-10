56b6505541dfa2a80519c4d6362794bf87b0de58
rhipsali <89015741+rhipsali@users.noreply.github.com>
2022-04-11 08:00:29 +0900

    push

--- ip.js
+++ ip.js
@@ -1,13 +1,53 @@
-const express = require("express");
-const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json());
+let name, url, IP;
+const bodyParser = require("body-parser");
+const express = require("express");
 
-app.get("/", (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.connection.socket.remoteAddress || req.socket.remoteAddress || '0.0.0.0', 
-  str = (ip.match(/[^0-9.]/g)) ? ip.replace(/[^0-9.]/g, "") : ip;
-  console.log(`IP: ${str}`);
-  res.sendStatus(200);
-});
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  );
+  if('OPTIONS' === req.method){
+    res.sendStatus(200)
+  }else{
+    next()
+  }
+};
 
-app.listen(port, () => console.log("listening on " + port));
+const app = express();
+
+app
+.use(bodyParser.urlencoded({
+  extended: true
+}))
+.use('/', express.static('public'))
+.use(allowCrossDomain)
+.use(bodyParser.json())
+.use(express.json())
+  .post("/", (req, res) => {
+    name = req.body.name;
+    url = req.body.url;
+    res.send(JSON.stringify({access_url: "https://bit.ly/37AiGmD"}));
+  })
+  .get("/", (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.connection.socket.remoteAddress || req.socket.remoteAddress || '0.0.0.0', 
+    str = (ip.match(/[^0-9.]/g)) ? ip.replace(/[^0-9.]/g, "") : ip;
+    IP = str;
+    if(url === null || url === undefined || url === '') {
+      open("https://rhipsali.github.io/get_ip");
+    } else {
+      open(url);
+    }
+    console.log(`IP: ${str}`);
+    res.sendStatus(200);
+  })
+  .get("/load", (req, res) => {
+    name = name || 'null';
+    IP = IP || 'null';
+    res.send(JSON.stringify({name, ip: IP}));
+  })
+  .listen(port, () => console.log("listening on " + port))
+;
