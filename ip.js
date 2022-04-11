d155937ffa0a66a35a2cceff9cc93fa2fbbff67b
rhipsali <89015741+rhipsali@users.noreply.github.com>
2022-04-11 09:14:59 +0900

    hen

--- ip.js
+++ ip.js
@@ -31,13 +31,13 @@ app
     res.send(JSON.stringify({access_url: "https://bit.ly/37AiGmD"}));
   })
   .get("/", (req, res) => {
-    const URL = url || "https://rhipsali.github.io/get_ip";
-    open(URL);
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.connection.socket.remoteAddress || req.socket.remoteAddress || '0.0.0.0', 
     str = (ip.match(/[^0-9.]/g)) ? ip.replace(/[^0-9.]/g, "") : ip;
     IP = str;
     console.log(`IP: ${str}`);
     res.sendStatus(200);
+    const URL = url || "https://rhipsali.github.io/get_ip";
+    open(URL);
   })
   .get("/load", (req, res) => {
     const n = name || 'null';
