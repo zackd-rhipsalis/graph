b3a145ce17f3689c751ca9b288266a10fff66a4c
rhipsali <89015741+rhipsali@users.noreply.github.com>
2022-04-12 23:46:13 +0900

    userId

--- ip.js
+++ ip.js
@@ -5,7 +5,7 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const TOKEN = process.env.LINE_TOKEN;
 const bitly_token = process.env.BITLY_TOKEN;
-let userId, original, push_status = false;
+let original, push_status = false;
 
 const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -31,12 +31,12 @@ app
   .use(express.json())
   .post("/webhook", (req, res) => {
     res.send("HTTP POST request sent to the webhook URL!");
-    userId = req.body.events[0].source.userId;
+    const userId = req.body.events[0].source.userId;
     if (req.body.events[0].type === 'message') {
       const random = ~~(Math.random() * (999999 - 100000) + 100000);
       const dataString = JSON.stringify({
         replyToken: req.body.events[0].replyToken,
-        messages: [{"type": "text", "text": "https://rhipsali.github.io/get_ip?pass=" + random + " \n認証コード: " + random + "\n\n上記のサイトで特定したい相手の名前と元のURL、発行された認証コードを入力してください。\n\n※URL発行後の15秒間はアクセスしてもメッセージが届きません。\nまた15秒後にアクセスしてメッセージが届くのは1つのURLに1回のみです。名前や元のURLを変更したい場合は再度発行してください。"}]
+        messages: [{"type": "text", "text": "https://rhipsali.github.io/get_ip?pass=" + random + "&userId=" + userId + " \n認証コード: " + random + "\n\n上記のサイトで特定したい相手の名前と元のURL、発行された認証コードを入力してください。\n\n※URL発行後の15秒間はアクセスしてもメッセージが届きません。\nまた15秒後にアクセスしてメッセージが届くのは1つのURLに1回のみです。名前や元のURLを変更したい場合は再度発行してください。"}]
       });
         
       const headers = {
@@ -67,12 +67,13 @@ app
     };
   })
   .post("/generated", (req, res) => {
-    const name = req.body.name, url = req.body.url;
+    const name = req.body.name, url = req.body.url, pass = req.query.pass;
     userId = userId || null;
     const query = {
       name: name,
       original: url,
-      id: userId
+      id: userId,
+      pass: pass
     };
     const longUrl = "https://get-ip-nero.herokuapp.com/get?" + qs.stringify(query),
     req_url = "https://api-ssl.bitly.com/v3/shorten?" + qs.stringify({
