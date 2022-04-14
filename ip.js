1c7d7da7bdcb4388aa8638d9964730a43b5a114e
rhipsali <89015741+rhipsali@users.noreply.github.com>
2022-04-14 22:32:36 +0900

    huh

--- ip.js
+++ ip.js
@@ -101,7 +101,6 @@ app
     // push msg
     if ( !id || 
       req.headers["accept-language"] !== 'ja' ||
-      req.headers["user-agent"] === "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1" ||
       req.headers["user-agent"] === 'bitlybot/3.0 (+http://bit.ly/)' 
     ) push_status = false; else push_status = true;
     if(push_status) {
