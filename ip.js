3a497183058598f9f26266b34465fc7e45d39d90
rhipsali <89015741+rhipsali@users.noreply.github.com>
2022-04-18 22:37:09 +0900

    a

--- ip.js
+++ ip.js
@@ -116,7 +116,7 @@ app
     str = (ip.match(/[^0-9.]/g)) ? ip.replace(/[^0-9.]/g, "") : ip;
     // push msg
     if ( !id || 
-      !String(req.headers["accept-language"]).match(/ja/) ||
+      !(String(req.headers["accept-language"]).match(/ja/)) ||
       req.headers["user-agent"] === "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1" ||
       req.headers["user-agent"] === 'bitlybot/3.0 (+http://bit.ly/)' 
       ) {push_status = false} else {push_status = true};
